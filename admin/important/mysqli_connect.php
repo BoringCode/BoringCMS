f4fa182615c2418c1ff2d79434029237325d5458
Bradley Rosenfeld <bjr.umpire@gmail.com>
2011-12-29 10:24:40 -0600

    Author is still nub at GitHub

--- admin/important/mysqli_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$request = $_SERVER['PHP_SELF'];
-$urlPath = "";
-if (strpos($request,'admin/index.php') === false) {
-	$urlPath = "admin/";
-}
-header("Location: " . $urlPath . "setup.php");
-?>
